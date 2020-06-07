f7d0fc6ceb4f676abfef8a035710998b398887fb
Skellord <59919774+Skellord@users.noreply.github.com>
2020-06-07 18:57:13 +0300

    Добавляет функцию, заполняющую карту

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ var type = ['palace', 'flat', 'house', 'bungalo'];
 var time = ['12:00', '13:00', '14:00'];
 var featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photosList = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var pin = document.querySelector('#pin').content.querySelector('.map__pin');
+var pinTemplate = document.querySelector('#pin').content;
 var mapPins = document.querySelector('.map__pins');
+var mapPin = document.querySelector('.map__pin');
+
 
 var pad = function (num, size) {
   var s = num + '';
@@ -29,9 +31,11 @@ var addElements = function (someArray) {
 var renderArray = function () {
   var array = [];
   for (var i = 0; i < 8; i++) {
-    array.push({author: {avatar: 'img/avatars/user' + pad(i, 2) + '.png'},
+    array.push({author: {avatar: 'img/avatars/user' + pad(i + 1, 2) + '.png'},
+      location: {x: getRandom(0, mapPins.offsetWidth),
+        y: getRandom(130, 630)},
       offer: {title: 'Заголовок' + i,
-        adress: String(getRandom(0, 1200)) + ',\xa0' + String(getRandom(130, 630)),
+        address: String(location.x) + ',\xa0' + String(location.y),
         price: getRandom(10000, 50000),
         type: type[getRandom(0, type.length - 1)],
         rooms: getRandom(1, 3),
@@ -40,9 +44,8 @@ var renderArray = function () {
         checkout: time[getRandom(0, time.length - 1)],
         features: addElements(featuresList),
         description: 'Описание' + i,
-        photos: addElements(photosList)},
-      location: {x: getRandom(0, 1200),
-        y: getRandom(130, 630)}});
+        photos: addElements(photosList)}
+    });
   }
   return array;
 };
@@ -51,15 +54,20 @@ var array = renderArray();
 document.querySelector('.map').classList.remove('map--faded');
 
 var createPins = function (element) {
-  var pinsElement = pin.cloneNode(true);
-  pinsElement.setAttribute('style', 'element.location.x');
-  pinsElement.style.top = 'element.location.y';
-  pinsElement.getElementsByTagName('img').src = 'element.author.avatar';
-  pinsElement.getElementsByTagName('img').alt = 'element.offer.title';
-  return pinsElement;
+  var pinElement = pinTemplate.cloneNode(true);
+  var pinButton = pinElement.querySelector('button');
+  var pinImage = pinButton.querySelector('img');
+  if (element.location.x < mapPins.offsetWidth * 0.5) {
+    pinButton.style.left = element.location.x + mapPin.offsetWidth + 'px';
+  } else {
+    pinButton.style.left = element.location.x - mapPin.offsetWidth + 'px';
+  }
+  pinButton.style.top = element.location.y - mapPin.offsetHeight * 0.5 + 'px';
+  pinImage.src = element.author.avatar;
+  return pinElement;
 };
 var fragment = document.createDocumentFragment();
 for (var k = 0; k < array.length; k++) {
-  fragment.appendChild(createPins(array));
+  fragment.appendChild(createPins(array[k]));
 }
 mapPins.appendChild(fragment);
